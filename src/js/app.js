2a2b8e87911df2a4039e1e563d5419fe4abe795d
shawn <shawn@naturalprofessional.com>
2017-08-09 22:34:13 -0700

    Add error message if try to play on not-mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,15 @@
-var Numbers = require('./numbers')
-var gameSetUp = require('./game')
-var KonvaGame = require('./konva-game')
+// Try Modernizr
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+	console.log("Playing")
+	var Numbers = require('./numbers')
+	var gameSetUp = require('./game')
+	var KonvaGame = require('./konva-game')
 
-
+} else {
+	console.log("Not a mobile device")
+  document.getElementById('full-screen').innerHTML = "<p>Run Hungry Monster on a mobile device, or in Chrome Dev Tools with mobile simulator</p>";
+  document.getElementById('full-screen').setAttribute('class', 'wrong-device');
+}
 
 document.getElementById("play-button").addEventListener("click", reloadPage);
 
