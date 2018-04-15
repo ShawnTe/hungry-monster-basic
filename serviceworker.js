4bca63c92a1062f68c490348e8d6613c6b95731b
shawn <shawn@naturalprofessional.com>
2018-04-14 20:43:20 -0700

    Initial offline setup

---
+++ serviceworker.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  var filesToCache = [
+    '.',
+    'src/images/blue-monster-32x32.png',
+    'src/css/style.css',
+    'bin/app.bundle.js',
+    'index.html',
+    'offline.html',
+    '404.html'
+  ];
+
+
+
+
+})();
